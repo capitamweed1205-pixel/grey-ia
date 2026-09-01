c60ce65b4b0c60fd84a4e12faedd79019f2b7ae2
capitamweed1205-pixel <capitamweed1205@gmail.com>
2026-09-01 10:54:17 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        grey: { 950: "#080808", 900: "#121212", 800: "#1c1c1c" }
+      },
+    },
+  },
+  plugins: [],
+}
