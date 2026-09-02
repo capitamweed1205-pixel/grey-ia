823cbd7230d2764802b137acafe04ddabc4f2dee
capitamweed1205-pixel <capitamweed1205@gmail.com>
2026-09-02 08:42:35 +0200

    Simplify layout by removing Providers and UIWrapper
    Removed Providers and UIWrapper components from layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import './globals.css'
-import { Providers } from "./providers"
-import UIWrapper from "./ui-wrapper"
 
 export const metadata = {
   title: 'GREY IA',
@@ -13,16 +11,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
       </head>
-      <body style={{ margin: 0, backgroundColor: 'black' }}>
-        <Providers>
-          <UIWrapper>
-            {children}
-          </UIWrapper>
-        </Providers>
+      <body style={{ margin: 0, backgroundColor: 'black', color: 'white' }}>
+        {children}
       </body>
     </html>
   )
-
-
-
-
+}
